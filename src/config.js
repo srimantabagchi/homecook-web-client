4df62c016e349cfb7a4cd1d637fdb42c6376b4a2
Srimanta Bagchi <sbagchi@loyalty.com>
2020-02-03 21:40:44 -0500

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-file-upload-folder"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://5av36gzh3m.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_FNXemQy5q",
+    APP_CLIENT_ID: "1idaoktd6e022ruk1ohpuj9436",
+    IDENTITY_POOL_ID: "us-east-1:ee7b91b1-7d65-428e-a429-9f5554dc3e5f"
+  }
+};
